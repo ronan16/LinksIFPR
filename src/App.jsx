e0310dcdfc3ab81db6038a3af2d409b70d12f941
profronan16 <ronan.16@hotmail.com>
2024-08-19 19:22:54 -0300

    Adicionado os projetos certos

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,43 @@ import "./App.css";
 import LinksPage from "./components/LinksPage";
 
 // Importando imagens
-import logoImage from "./assets/img/logo.png";
+import arte from "./assets/img/arte.png";
+import confraria from "./assets/img/confraria.png";
+import empatia from "./assets/img/empatia.png";
+import sustentabilidade from "./assets/img/sustentabilidade.png";
+
+// Importando PDF
+import artePDF from "./assets/pdf/EditalArte.pdf";
+import confrariaPDF from "./assets/pdf/EditalConfraria.pdf";
+import empatiaPDF from "./assets/pdf/EditalEmpatia.pdf";
+import sustentabilidadePDF from "./assets/pdf/EditalSustentabilidade.pdf";
 
 function App() {
   const [count, setCount] = useState(0);
   const projects = [
     {
-      name: "Projeto de Pesquisa A",
-      image: logoImage, // Usando a imagem importada
-      inscricaoLink:
-        "https://ifpr.edu.br/ivaipora/nossos-cursos/ensino-superior/bacharelado-em-sistemas-de-informacao/",
-      editalLink:
-        "https://ifpr.edu.br/ivaipora/wp-content/uploads/sites/15/2018/08/PPC-Bacharelado-em-Sistemas-de-Informacao-versao-final.pdf",
+      name: "Mais Empatia",
+      image: empatia, // Usando a imagem importada
+      inscricaoLink: "https://forms.gle/kyALk9BTZYTJyipM9",
+      editalLink: empatiaPDF,
     },
     {
-      name: "Projeto de Extensão B",
-      image: logoImage, // Usando a imagem importada
-      inscricaoLink:
-        "https://ifpr.edu.br/ivaipora/nossos-cursos/ensino-superior/bacharelado-em-sistemas-de-informacao/",
-      editalLink:
-        "https://ifpr.edu.br/ivaipora/wp-content/uploads/sites/15/2018/08/PPC-Bacharelado-em-Sistemas-de-Informacao-versao-final.pdf",
+      name: "Arte e Cultura: Formar e Transformar",
+      image: arte, // Usando a imagem importada
+      inscricaoLink: "https://forms.gle/a4RXQs3Tpbc9fYsCA",
+      editalLink: artePDF,
+    },
+    {
+      name: "Confraria do Vale do Ivaí",
+      image: confraria, // Usando a imagem importada
+      inscricaoLink: "https://forms.gle/YrNUPiu9ew8ETnzf9",
+      editalLink: confrariaPDF,
+    },
+    {
+      name: "À Procura da Sustentabilidade",
+      image: sustentabilidade, // Usando a imagem importada
+      inscricaoLink: "https://forms.gle/Lh5gX1eTDLJSSZfWA",
+      editalLink: sustentabilidadePDF,
     },
     // Adicione mais projetos conforme necessário
   ];
